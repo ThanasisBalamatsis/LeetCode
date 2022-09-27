d9082c1eae32bb3e225b087b980e6bf38726f1cc
Thanasis Balamatsis <thanasis.b@yahoo.com>
2022-09-27 23:18:29 +0300

    Add Solution.cs for Richest Customer Wealth to the corresponding folder

---
+++ C#/Richest Customer Wealth/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Richest_Customer_Wealth
+{
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int maxWealth = 0;
+
+            foreach (int[] account in accounts)
+            {
+                int wealth = account.Sum();
+                if (wealth > maxWealth)
+                    maxWealth = wealth;
+            }
+
+            return maxWealth;
+        }
+    }
+}
