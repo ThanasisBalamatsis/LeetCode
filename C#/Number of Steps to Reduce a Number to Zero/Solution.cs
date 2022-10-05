507130718f85baa9a524cb74e571ab5f5b4b14ff
Thanasis Balamatsis <thanasis.b@yahoo.com>
2022-10-05 21:49:32 +0300

    Add Solution for Number of Steps to Reduce a Number to Zero problem

---
+++ C#/Number of Steps to Reduce a Number to Zero/Solution.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ReductToZero
+{
+    public class Solution
+    {
+        public int NumberOfSteps(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                count++;
+                if (num % 2 == 0)
+                    num = num / 2;
+                else
+                    num--;
+            }
+            return count;
+        }
+    }
+}
