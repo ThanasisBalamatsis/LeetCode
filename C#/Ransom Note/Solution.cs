00e1feb8adce2414600d73e861fab1f61e45c96d
Thanasis Balamatsis <thanasis.b@yahoo.com>
2022-10-13 23:05:00 +0300

    Add Ransom Note solution to C# folder

---
+++ C#/Ransom Note/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Solution
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> magazineChars = magazine.ToList();
+            foreach (char character in ransomNote)
+            {
+                if (!magazineChars.Contains(character))
+                    return false;
+                else
+                    magazineChars.Remove(character);
+            }
+            return true;
+        }
+    }
+}
