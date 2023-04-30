22805da8df328693ce1da8287cc8ec95d66c1d60
Thanasis Balamatsis <thanasis.b@yahoo.com>
2023-04-30 23:36:26 +0300

    Add Search Insert Position Solution

---
+++ C#/Search Insert Position/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInsertPosition
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target || nums[i] > target)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+}
