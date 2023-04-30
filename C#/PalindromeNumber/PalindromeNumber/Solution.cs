b84f2fd77b1b75caf7814b10d2f77551898965da
Thanasis Balamatsis <thanasis.b@yahoo.com>
2023-04-30 19:05:12 +0300

    Add Palindrome Number Solution

---
+++ C#/PalindromeNumber/PalindromeNumber/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            var reversedString = new string(x.ToString().Reverse().ToArray());
+
+            return reversedString == x.ToString();
+        }
+    }
+}
